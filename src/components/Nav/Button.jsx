0a8c16ab41977112ec6bcb6959ea9c07f6de20d9
Rothiii <drome.emord@gmail.com>
2023-11-23 23:45:36 +0800

    Add things to do Components
    Now only 4 cols for desktop and 2 cols for mobile, in the furute I want to make4 cols & 2 rows for desktop and 2 cols & 2 rows for mobile. So the other 4 cards are hidden for mobile

--- src/components/Nav/Button.jsx
+++ src/components/Nav/Button.jsx
@@ -2,7 +2,7 @@
 function button() {
   return (
     <div>
-      <a className="btn btn-ghost bg-orange-500 text-white text-[3vw] md:text-[2vw] lg:text-[1vw]">Get In Touch</a>
+      <a className="btn btn-ghost cursor-pointer bg-orange-500 text-white text-[3vw] md:text-[2vw] lg:text-[1vw]">Get In Touch</a>
     </div>
   )
 }
