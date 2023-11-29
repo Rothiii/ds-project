4abe0567ab9d1c41913eee669c5581191cfbe1e9
AngelaCathe <11211013@student.itk.ac.id>
2023-11-29 22:39:44 +0800

    seperated HomePage contents to be in its own component, added page routing for the website

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,27 @@ import Escape from './components/Escape/Escape'
 import Things from './components/Thing/Things'
 import News from './components/News/News'
 import { Analytics } from '@vercel/analytics/react'
-
+import HomePage from './components/Home'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />
+    }
+  ])
   return(
-    <>
+    <RouterProvider router={router}></RouterProvider>
+    // <>
      
-     <Nav />
-     <Hero />
-     <Escape />
-     <Things />
-     {/* <News /> */}
-     <Analytics />
-    </>
+
+    //  <Nav />
+    //  <Hero />
+    //  <Escape />
+    //  <Things />
+    //  {/* <News /> */}
+    //  <Analytics />
+    // </>
   )
 }
 
