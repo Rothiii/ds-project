28e6e86b4f18a09e685a0db2f9c0116cc6096376
anisaaindraa <11211014@student.itk.ac.id>
2023-11-30 16:25:02 +0800

    updated mailing page content, added background for the mailing page

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,32 @@ import Escape from './components/Escape/Escape'
 import Things from './components/Thing/Things'
 import News from './components/News/News'
 import { Analytics } from '@vercel/analytics/react'
-
+import HomePage from './components/Home'
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Mailing from './components/Mailing'
 function App() {
-
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />
+    },
+    {
+      path: '/get-in-touch',
+      element: <Mailing />
+    }
+  ])
   return(
-    <>
+    <RouterProvider router={router}></RouterProvider>
+    // <>
      
-     <Nav />
-     <Hero />
-     <Escape />
-     <Things />
-     {/* <News /> */}
-     <Analytics />
-    </>
+
+    //  <Nav />
+    //  <Hero />
+    //  <Escape />
+    //  <Things />
+    //  {/* <News /> */}
+    //  <Analytics />
+    // </>
   )
 }
 
