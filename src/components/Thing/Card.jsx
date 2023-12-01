f02e9c934ad19ccb5218829ba60ac7e9f011ca5d
Rothiii <drome.emord@gmail.com>
2023-12-01 19:27:52 +0800

    Revert "Revert "Merge dev to main""

--- src/components/Thing/Card.jsx
+++ src/components/Thing/Card.jsx
@@ -5,7 +5,7 @@ function Card({gambar, caption, judul, desc}) {
       <div className="">
         <img src={gambar} alt={caption} className="rounded-2xl xl:rounded-3xl object-cover object-center w-[40vw] md:w-[45vw] lg:w-[25vw] xl:w-[17vw] xl:h-[15vw]"/>
       </div>
-      <h1 className="font-bold text-[4vw] md:text-[3vw] xl:text-[1.5vw]">{judul}</h1>
+      <h1 className="font-bold text-[4vw] md:text-[2vw] xl:text-[1.5vw]">{judul}</h1>
       <p className="text-[2.9vw] md:text-[1.68vw] xl:text-[0.9vw] line-clamp-5 xl:line-clamp-4">{desc}</p>
     </div>
     
