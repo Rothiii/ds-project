df50d1fb6b2ce25bb08659273578504e3f10b0f2
AngelaCathe <11211013@student.itk.ac.id>
2023-12-01 16:24:45 +0800

    another attempt to resolve deployment issues :)

--- src/components/flights/FlightCard.jsx
+++ src/components/flights/FlightCard.jsx
@@ -1,12 +1,12 @@
 
 function FlightCard({reflink, image, title, desc}) {
     return ( 
-        <div className="">
-            <a href={reflink} className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-xl dark:hover:bg-gray-100">
+            <a href={reflink}>
+            <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-xl dark:hover:bg-gray-100">
             <img className="rounded-t-lg" src={image} alt="" />
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-gray-700">{title}</h5>
+            </div>
             </a>
-        </div>
 
      );
 }
