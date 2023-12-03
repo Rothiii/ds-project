44b4c2306bed110b05ac3038e5d727a357a7a999
anisaaindraa <11211014@student.itk.ac.id>
2023-12-03 14:51:33 +0800

    fixed image bug

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,8 +5,8 @@ function About() {
     <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
         <div class="flex flex-wrap ">
             <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
-                <img src="https://i.postimg.cc/j5L5bX2d/pexels-andrea-piacquadio-3757946.jpg" alt=""
-                    class="relative z-40 object-cover w-full h-96 rounded-3xl" />
+                <img src="balikpapankota.jpeg" alt=""
+                    class="relative z-40 object-cover w-full h-100 rounded-3xl" />
             </div>
             <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0 ">
                 <h2 class="mb-4 text-4xl font-semibold text-blue-500 dark:text-gray-300">
