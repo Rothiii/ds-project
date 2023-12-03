bc09c6e023ef634f6fa352361eb6778f3a4cba02
anisaaindraa <11211014@student.itk.ac.id>
2023-12-03 14:59:56 +0800

    fixed spacing between contents in about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,10 @@ function About() {
   return (
     <div>
       <NavBar />
-    <section class="flex items-center bg-stone-100 xl:h-screen font-poppins dark:bg-gray-800 ">
+      <div className="bg-local bg-cover bg-center h-[6vh] overflow-hidden">
+            </div>
+          
+    <section class="flex items-center  xl:h-screen font-poppins dark:bg-gray-800 ">
     <div class="justify-center flex-1 max-w-6xl py-4 mx-auto lg:py-6 md:px-6">
         <div class="flex flex-wrap ">
             <div class="w-full px-4 mb-10 lg:w-1/2 lg:mb-0">
