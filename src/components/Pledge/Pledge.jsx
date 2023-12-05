c08ed3a14098a00a624fc62d285debcb68e6bc65
anisaaindraa <11211014@student.itk.ac.id>
2023-12-05 19:23:20 +0800

    updates pledge images into pledge page

--- src/components/Pledge/Pledge.jsx
+++ src/components/Pledge/Pledge.jsx
@@ -2,7 +2,7 @@ import Card from './Card.jsx'
 
 function News() {
   return (
-    <Card imgSrc={"./vite.svg"} title={"Janji Balikpapan"} description={"Apakah kamu mengunjungi Balikpapan? Jadilah seorang wisatawan yang bertanggung jawab dan ambil janji Balikpapan."} href="/pledge"/>
+    <Card imgSrc={"./vite.jpeg"} title={"Janji Balikpapan"} description={"Apakah kamu mengunjungi Balikpapan? Jadilah seorang wisatawan yang bertanggung jawab dan ambil janji Balikpapan."} href="/pledge"/>
   )
 }
 
